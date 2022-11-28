c627e801d447cef38b83926b17c3ef1fe15915f1
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-28 13:25:05 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -57,6 +57,7 @@ public class Main {
         }
 
         for (int i=2; i<= n/2; i++){
+            //for(int i=2; i<= (long) Math.sqrt(n); i++)
             if(n % i == 0){
                 return false;
             }
